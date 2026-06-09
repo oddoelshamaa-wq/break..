7e7e869b65af5aa8018b034252443c57cd7175e8
oddoelshamaa-wq <oddo.elshamaa@gmail.com>
2026-06-09 10:24:15 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'elshamaa-pwa-v8'; // غيرت رقم الإصدار عشان يحذف الكاش القديم
+const urlsToCache = [
+  '/break-./',
+  '/break-./index.html',
+  '/break-./manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
